4e00e9bc8257479338a23bfea56733169acfc4c8
amarakuruma <kouroumaamara99@gmail.com>
2026-06-30 23:23:02 +0300

    ajout fiche tracteur variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+nom = "tracteur BOT 01"
+vitesse_max = 25 
+autonomie = 150
+est_actif = True 
+print("=====FICHE TRACTEUR=====")
+print("nom:", nom ) 
+print("vitesse:", vitesse_max,"km/h")
+print("autonomie:", autonomie,"km")
+print("est actif:", est_actif)
+
+
+
